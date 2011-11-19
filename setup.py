8cb74059f213e5eb847ef4d3c20193517814fb5e
Dan Crosta <dcrosta@gmail.com>
2011-11-19 14:02:09 -0500

    add setup metadata

--- setup.py
+++ setup.py
@@ -4,7 +4,22 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 setup(
-    name='keystone',
+    name='Keystone',
+    description='A very simple, yet flexible, dynamic website framework',
+    version='0.1',
+    author='Dan Crosta',
+    author_email='dcrosta@late.am',
+    license='BSD',
+    url='https://github.com/dcrosta/keystone',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
     install_requires=[
         'werkzeug',
         'jinja2',
