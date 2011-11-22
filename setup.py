aa7e8742a6323cb2ad8ddccd07cb3b33e6185980
Dan Crosta <dcrosta@gmail.com>
2011-11-21 20:38:15 -0500

    depend on argparse under py27

--- setup.py
+++ setup.py
@@ -2,6 +2,17 @@ from distribute_setup import use_setuptools
 use_setuptools()
 
 from setuptools import setup, find_packages
+from sys import version_info
+
+REQUIRES = [
+    'werkzeug',
+    'jinja2',
+]
+
+if version_info < (2, 7):
+    # no argparse in 2.6 standard
+    REQUIRES.append('argparse')
+
 
 setup(
     name='Keystone',
@@ -20,10 +31,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    install_requires=[
-        'werkzeug',
-        'jinja2',
-    ],
+    install_requires=REQUIRES,
     packages=find_packages(),
     entry_points={
         'console_scripts': [
